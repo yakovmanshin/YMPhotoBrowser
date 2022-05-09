3961a16652eeee0a2138939fdb03fb734381e771
Yakov Manshin <git@yakovmanshin.com>
2022-05-09 23:33:47 +0400

    Deleted `UIImage (animatedGIF)`

--- SKPhotoBrowser/SKPhoto.swift
+++ SKPhotoBrowser/SKPhoto.swift
@@ -107,7 +107,7 @@ open class SKPhoto: NSObject, SKPhotoProtocol {
                     return
                 }
 
-                if let data = data, let response = response, let image = UIImage.animatedImage(withAnimatedGIFData: data) {
+                if let data = data, let response = response, let image = UIImage(data: data) {
                     if self.shouldCachePhotoURLImage {
                         if SKCache.sharedCache.imageCache is SKRequestResponseCacheable {
                             SKCache.sharedCache.setImageData(data, response: response, request: task?.originalRequest)
